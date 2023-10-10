6ae2e70f2caee698fcaa0da6778aae1078bc9cad
malleswarialajingi <147472888+malleswarialajingi@users.noreply.github.com>
2023-10-10 11:20:24 +0530

    Add files via upload

---
+++ Area and Perimeter of a Square.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	//scanf("side of a square = s)";
+	//scanf("perimeter of a square = p)";
+	//scanf("area of a square = a)";
+	int s = 5;
+	int p = 4*s;
+	int a = s*s;
+	printf("%d",p);
+	printf("\n%d" , a);
+}
